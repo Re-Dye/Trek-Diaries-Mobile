0ef7abc151738c70eb807fc16c244fcac177b0b4
Re-Dei <hridaya.pradhan123@gmail.com>
2024-05-24 21:15:18 +0545

    feat: feed api

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import React from 'react';
 import FeedCard from '@/components/FeedCard';
 import { SafeAreaView } from 'react-native-safe-area-context';
---
+++ app/api/feed+api.ts
@@ -0,0 +1,29 @@
+import { getFeed } from "@/lib/db/actions";
+
+export async function GET(req: Request) {
+  try {
+    const params = new URL(req.url).searchParams;
+    const userId = params.get("userId");
+    const _limit = params.get("limit");
+    const last = params.get("last");
+
+    if (!userId || !_limit || !last) {
+      return Response.json("Invalid Request", { status: 400 });
+    } else {
+      const limit = +_limit;
+
+      if (isNaN(limit)) {
+        return Response.json("Invalid Request", { status: 400 });
+      }
+
+      const { posts, next } = await getFeed(userId, limit, last);
+
+      return Response.json(JSON.stringify({ posts, next }), {
+        status: 200,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    return Response.json("Internal Server Error", { status: 500 });
+  }
+}
