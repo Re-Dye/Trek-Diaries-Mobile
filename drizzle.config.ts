39f2726c65017c6e93eef064217cdf7fc3a36cc7
Re-Dei <hridaya.pradhan123@gmail.com>
2024-06-02 13:05:31 +0545

    fix: env path on drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({ path: './.env.local' });
 
 export default {
   schema: './lib/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DB_URL!,
   },
 } satisfies Config;
